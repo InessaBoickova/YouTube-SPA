4c6c7be66a451f7e41e3c88e660dfa9bf19f9bb1
Inessa <inessa.boickova@yandex.ru>
2022-12-06 01:06:43 +0300

    create Video Search page

--- src/component/VideoSearch/videoSearch.jsx
+++ src/component/VideoSearch/videoSearch.jsx
@@ -1,8 +1,26 @@
+import './videoSearch.sass'
 import Navigation from "../Navigation/navigation";
+import { SearchOutlined } from '@ant-design/icons';
+import { Input,Typography,Button, Space } from 'antd';
+const { Title } = Typography;
 
 const VideoSearch = ()=>{
     return(
-        <Navigation/>
+        <div className="video_search">
+            <Navigation/>
+            <Title level={2}>
+                Поиск видео
+            </Title>
+            <div className='video_search-wrapper'>
+                <Input placeholder="Поиск видео" className='video_search-input' />
+                <Space wrap>
+                    <Button type="primary" icon={<SearchOutlined />}>
+                        Search
+                    </Button>
+                </Space>
+            </div>
+        </div>
+       
     )
 }
 
